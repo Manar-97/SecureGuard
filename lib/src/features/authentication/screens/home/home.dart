a2bf7713a417d499bb6b520c4da326ba236b0122
Manar-97 <151880750+Manar-97@users.noreply.github.com>
2024-11-29 19:04:02 +0200

    Update home.dart

--- lib/src/features/authentication/screens/home/home.dart
+++ lib/src/features/authentication/screens/home/home.dart
@@ -1,127 +1,212 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart'; // إضافة المكتبة الخاصة بالأذونات
 import '../../../../../data/api/malware_scan_api.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
   static const String routeName = "home";
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String scanResult = "Press the button to scan all files.";
-  bool isScanning = false;
-  double progress = 0.0;
+  final VirusTotalAPI virusTotalAPI = VirusTotalAPI();
+  String result = "Scan result will appear here.";
+  bool isLoading = false;
+  String fileReport = "";
+
+  // دالة لاختيار الملف بعد التحقق من الأذونات
+  Future<void> pickAndScanFile(BuildContext context) async {
+    // طلب إذن الوصول إلى التخزين
+    PermissionStatus status = await Permission.storage.request();
+    if (status.isGranted) {
+      // إذا تم منح الإذن، اختر الملف
+      FilePickerResult? pickedFile = await FilePicker.platform.pickFiles();
+      if (pickedFile != null && pickedFile.files.single.path != null) {
+        String filePath = pickedFile.files.single.path!;
+        File file = File(filePath);
+
+        setState(() {
+          isLoading = true;
+          result = "Uploading and scanning...";
+        });
+
+        String? fileHash = await VirusTotalAPI.uploadFile(file);
+
+        if (fileHash != null) {
+          await Future.delayed(const Duration(seconds: 10)); // انتظار النتيجة
+
+          String? report = await VirusTotalAPI.getFileReport(fileHash);
+          setState(() {
+            if (report != null) {
+              // تحليل التقرير لمعرفة عدد النتائج المشبوهة
+              int maliciousCount = _extractMaliciousCount(report);
+
+              if (maliciousCount == 0) {
+                fileReport = "The file is safe. No threats detected.";
+              } else {
+                fileReport =
+                    "The file contains $maliciousCount threat(s). It is malicious.";
+              }
+              result = "File uploaded successfully.";
+            } else {
+              fileReport = "No report found.";
+              result = "Failed to retrieve the scan report.";
+            }
+            isLoading = false;
+          });
+
+          // إظهار تقرير الفحص في مربع حوار
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("Scan Report",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                content: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      const Divider(),
+                      const SizedBox(height: 10),
+                      fileReport.isNotEmpty
+                          ? Text(
+                              fileReport,
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.grey[400]),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("OK"),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          setState(() {
+            result = "Failed to upload file.";
+            isLoading = false;
+          });
+        }
+      } else {
+        setState(() {
+          result = "No file selected.";
+          isLoading = false;
+        });
+      }
+    } else {
+      // إذا تم رفض الإذن
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Permission to access files is denied.")),
+      );
+    }
+  }
+
+  // دالة لتحليل التقرير واستخراج عدد التهديدات
+  int _extractMaliciousCount(String report) {
+    try {
+      Map<String, dynamic> jsonReport = jsonDecode(report);
+      return jsonReport['malicious'] ?? 0; // افترض أن المفتاح "malicious" موجود
+    } catch (e) {
+      debugPrint("Error parsing report: $e");
+      return 0;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Spacer(),
-            Center(
-              child: Text(
-                scanResult,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            const Spacer(),
-
-            // شريط التقدم أثناء الفحص
-            isScanning
-                ? Column(
-                    children: [
-                      CircularProgressIndicator(
-                        value: progress,
-                        color: Colors.blue,
-                        backgroundColor: Colors.grey[300],
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        "Scanning... ${(progress * 100).toStringAsFixed(0)}%",
-                        style: const TextStyle(color: Colors.black54),
-                      ),
-                    ],
-                  )
-                : Container(),
-
-            const Spacer(),
-
-            // زر "Scan All Files"
-            SizedBox(
-              height: 170,
-              width: 170,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                  ),
-                  backgroundColor: Colors.brown,
-                ),
-                onPressed: isScanning
-                    ? null
-                    : () async {
-                        await requestPermission(); // طلب الأذونات قبل بدء الفحص
-                        startScan(); // بدء الفحص
-                      },
-                child: Text(
-                  isScanning ? "Scanning..." : "Scan All Files",
-                  style: const TextStyle(fontSize: 18,color: Colors.black),
-                ),
-              ),
-            ),
-
-            const Spacer(),
-          ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("Virus Scanner")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              isLoading
+                  ? const LinearProgressIndicator() // شريط التقدم
+                  : Column(
+                      children: [
+                        Text(
+                          result,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey),
+                        ),
+                        const SizedBox(height: 20),
+                        Card(
+                          elevation: 10,
+                          shadowColor: Colors.black.withOpacity(0.3),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                const Icon(
+                                  Icons.check_circle_outline,
+                                  color: Colors.green,
+                                  size: 40,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  "Scan Report",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge
+                                      ?.copyWith(color: Colors.blue),
+                                ),
+                                const SizedBox(height: 10),
+                                const Divider(),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8.0),
+                                  child: Text(
+                                    fileReport.isEmpty
+                                        ? "No report available yet."
+                                        : fileReport,
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey[600]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        ElevatedButton.icon(
+                          onPressed: () => pickAndScanFile(context),
+                          icon: const Icon(Icons.upload_file),
+                          label: const Text("Pick and Scan File"),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                            textStyle: const TextStyle(fontSize: 16),
+                            backgroundColor: Colors.blue[900],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50)),
+                          ),
+                        ),
+                      ],
+                    ),
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  // دالة لتحديث التقدم خلال الفحص (محاكاة)
-  Future<void> startScan() async {
-    setState(() {
-      isScanning = true;
-      progress = 0.0; // إعادة تعيين التقدم عند بداية الفحص
-    });
-
-    // محاكاة عملية الفحص: يتم تأخير التقدم بشكل تدريجي لتمثيل عملية الفحص
-    await Future.delayed(const Duration(seconds: 1));
-    for (int i = 0; i <= 100; i++) {
-      await Future.delayed(
-          const Duration(milliseconds: 30)); // تأخير لتحديث شريط التقدم
-      setState(() {
-        progress = i / 100; // تحديث شريط التقدم
-      });
-    }
-
-    // بعد إتمام الفحص، استدعاء الـAPI
-    String result = await MalwareScanApi.scanAllFiles();
-    setState(() {
-      scanResult = result; // تحديث النتيجة بناءً على الرد من الـAPI
-      isScanning = false; // إيقاف شريط التقدم بعد انتهاء الفحص
-    });
-  }
-
-  // طلب إذن للوصول إلى التخزين
-  Future<void> requestPermission() async {
-    final status = await Permission.storage.request();
-    if (status.isGranted) {
-      print("Permission granted!");
-    } else if (status.isDenied) {
-      print("Permission denied!");
-    } else if (status.isPermanentlyDenied) {
-      openAppSettings(); // فتح إعدادات التطبيق لتمكين الأذونات
-    }
   }
 }
