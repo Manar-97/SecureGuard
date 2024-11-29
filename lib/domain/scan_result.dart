051ca17deb54a458cd1d1eab76d3ee2f956f8c4a
Manar-97 <151880750+Manar-97@users.noreply.github.com>
2024-11-29 19:10:10 +0200

    Update scan_result.dart

--- lib/domain/scan_result.dart
+++ lib/domain/scan_result.dart
@@ -1,13 +1,1 @@
-class ScanResult {
-  final String status;
-  final String details;
 
-  ScanResult({required this.status, required this.details});
-
-  factory ScanResult.fromJson(Map<String, dynamic> json) {
-    return ScanResult(
-      status: json['status'],
-      details: json['details'],
-    );
-  }
-}
